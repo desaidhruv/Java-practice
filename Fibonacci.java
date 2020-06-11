9d0c46cfdd7fbf6c5a49dc1362e9d92e5a5a73b6
Dhruv Desai <dhruvshah28@gmail.com>
2020-06-11 17:53:40 +0530

    Fibonacci Updated

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,7 +5,7 @@ public class Fibonacci {
         int n1 = 0, n2 = 1, n3; //Initialize 0, 1
         int count;
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter the total number of Fibonacci series you want to print:"); // Input from user
+        System.out.print("Enter the total number of Fibonacci series you want to print:"); // Input from user.
         count = s.nextInt();
         System.out.print(n1 +" "+ n2); // Print 0 and 1
         for (int i = 2; i < count; i++) {  // loop begin from 2 because 0 and 1 already printed.
