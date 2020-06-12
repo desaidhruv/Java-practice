1598ac3a3b694b239ea155c86404e8a483eed98f
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-06-12 19:22:39 +0530

    Array of Objects
    Another Example for array of Objects

---
+++ Hrithik/Example2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Example2 {
+    String name;
+    String designation;
+    Float salary;
+
+    void accept() {
+        Scanner sc = new Scanner(System.in);
+        name = sc.nextLine();
+        designation = sc.nextLine();
+        salary = sc.nextFloat();
+    }
+    void show(){
+        
+        String p = "HOD || Head of the department";
+        if(p.equals(designation)){
+        System.out.println("Name: " + name);
+        System.out.println("Designation: " + designation);
+        System.out.println("Salary: "+ salary);
+        }
+    }
+        public static class Demo {
+            public static void main(final String[] args) {
+
+                Example2 E[] = new Example2[3];
+                for(int i = 0; i < 3; i++){
+                    E[i] = new Example2();
+                }
+                System.out.println("Enter details of the faculty");
+                for(int i= 0; i < 3; i++){
+                    E[i].accept();
+                }
+                System.out.println("Details of Faculty");
+                for(int i= 0; i < 3; i++){
+                    E[i].show();
+                }
+            }
+        }
+    }
