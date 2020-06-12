25a8e4726f3e78e50a48275e6cc42402a5fb4197
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-06-12 17:19:58 +0530

    practicing array of objects
     I have solved this example using array of object just to understand the concept more and more .

--- Hrithik/Example.java
+++ Hrithik/Example.java
@@ -1,7 +1,37 @@
 import java.util.*;
 
 public class Example {
-    public static void main(String[] args) {
-        
+        int no;
+        float bal;
+
+        public void accept() {
+            final Scanner sc = new Scanner(System.in);
+            System.out.println("Enter account number and balance");
+            no = sc.nextInt();
+            bal = sc.nextFloat();
+        }
+
+        public void result() {
+            if (bal > 5000) {
+                System.out.println("Account number: " + no);
+                System.out.println("Balance in the account is: " + bal);
+            }
+        }
+
+        public static class Demo {
+            public static void main(final String[] args) {
+                final Example E[] = new Example[2];
+        for (int i = 0; i < 2; i++) {
+            E[i] = new Example();
+        }
+        System.out.println("Enter details of the Account holder");
+        for(int i = 0; i < 2; i++){
+            E[i].accept();
+        }
+        System.out.println("Details of the Account holder");
+        for(int i = 0; i < 2; i++){
+            E[i].result();
+        }
+        }
     }
 }
