ff3c1b9a3ba10ec79e9c9f6195cfd84bd88b4ad5
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-06-13 21:01:11 +0530

    Inheritance
    This code helps to uderstand the concept of inheritance.

---
+++ Hrithik/Inheritance.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Inheritance {
+    String name, address;
+
+    void accept(){
+        final Scanner sc = new Scanner(System.in);
+        name = sc.nextLine();
+        address = sc.nextLine();
+        sc.close();
+    }
+
+    void show() {
+        System.out.println("Name of the branch: " + name);
+        System.out.println("Address of the branch: " + address);
+    }
+
+    public class Account extends Inheritance {
+        int id, balance;
+
+        void enter() {
+            final Scanner sc = new Scanner(System.in);
+            id = sc.nextInt();
+            balance = sc.nextInt();
+            sc.close();
+        }
+
+        void display() {
+            System.out.println("Account id: " + id);
+            System.out.println("Balance: " + balance);
+        }
+    }
+    public static class Demo {
+        public static void main(String[] args) {
+            Account a1 = new Account();
+                a1.accept();
+                a1.enter();
+                a1.show();
+                a1.display();
+            }
+        }
+    }
+
