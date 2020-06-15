fb004c1059b9b02157286927624cd67b7f8fcb7a
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-06-15 21:22:08 +0530

    Sum of divisors
    This code helps to find the sum of  divisors  of the number enterd by the user

---
+++ Hrithik/Interface.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Interface {
+    public static void main(String[] args){
+        System.out.println("Enter number whose sum you want");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int j = 0;
+        sc.close();
+        for (int i =1; i <= number; i++){
+            if(number%i == 0){
+                j = j + i;
+            }
+        }
+        System.out.println("Sum of divisors:" + j);
+    }
+}
