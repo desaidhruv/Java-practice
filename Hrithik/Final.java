3217f383d96169fba9289af0d54f20fe2cb9a972
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-06-17 23:22:43 +0530

    Final keyword 1/2
    This is just the basic concept of final keyword with an example of integer.

---
+++ Hrithik/Final.java
@@ -0,0 +1,8 @@
+public class Final {
+    public static void main(String[] args) {
+    final int x = 5;
+    int y = 50;
+        System.out.println("Value of x: " + x);
+        System.out.println("Value of y: " + y);
+        }
+    }
