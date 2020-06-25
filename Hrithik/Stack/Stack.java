35aed82db9151de54127959b5f3d4b0757c30ed7
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-06-25 23:33:00 +0530

    Stack operation
    This code is used to check if the expression is balanced expression or not.

---
+++ Hrithik/Stack/Stack.java
@@ -0,0 +1,44 @@
+ package Stack.HackerRank;
+ import java.io.*;
+ import java.util.*;
+ import java.text.*;
+ import java.math.*;
+ import java.util.regex.*;
+
+ public class Stack {
+
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         while(scanner.hasNextLine()) {
+             Stack<Character> stack = new Stack<>();
+             String line = scanner.nextLine();
+             for(char c : line.toCharArray()) {
+                 if(c == '{' || c == '(' || c == '[') {
+                 stack.push(c);
+                 continue;
+                 }                    
+                
+                 if(c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                     stack.pop();
+                     continue;
+                 }
+                
+                 if(c == ')' && !stack.isEmpty() && stack.peek() == '('){
+                     stack.pop();
+                     continue;
+                 }
+                
+                 if(c == ']' && !stack.isEmpty() && stack.peek() == '['){
+                     stack.pop();
+                     continue;
+                 }
+                
+//                 if(c == '}' || c == ')' || c == ']') {
+//                     stack.push(c);
+//                     break;
+//                 }   
+//             }
+//             System.out.println(stack.isEmpty());
+//         }
+//     }
+// }
