3345c4a11a79be03f0679a7749297a927ef34b80
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-07-01 21:00:18 +0530

    Max and Min in an array

---
+++ Hrithik/Array/Max_Min.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+public class Max_Min {
+
+    public static int Max(int[] arr){
+        int max = 0;
+        for(int k: arr){
+            if(k > max){
+                max = k;
+            }
+        }
+        return max;
+    }
+
+    public static int Min(int[] arr){
+        int min = arr[0];
+        for(int k: arr){
+            if(k < min){
+                min = k;
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i< n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int a = Max(arr);
+        int b = Min(arr);
+        System.out.println("Max: " + a);
+        System.out.println("Min: " + b);
+        sc.close();
+    }
+}
