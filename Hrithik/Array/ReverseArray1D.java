a9c37b286ca2df19f308f80f563a88ed7b0d845a
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-07-01 21:01:34 +0530

    Reverse of an array

---
+++ Hrithik/Array/ReverseArray1D.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class ReverseArray1D {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int count = sc.nextInt();
+    int[] arr = new int[count];
+        for(int i = 0; i < count; i++){
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        System.out.println("Reversed Array: ");
+        for(int i = arr.length-1; i >= 0; i--){
+            System.out.print(" " + arr[i]);
+        }
+    }
+	
+}
