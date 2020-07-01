4cf5e8463ab0a23904a47c856dd807dde94ba359
Dhruv Desai <dhruvshah28@gmail.com>
2020-07-01 20:08:01 +0530

    Arrays Updated

--- Dhruv/Arrays/OccurencesArray.java
+++ Dhruv/Arrays/OccurencesArray.java
@@ -12,6 +12,7 @@ public class OccurencesArray {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = s.nextInt();
         }
+        s.close();
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] == occ){
