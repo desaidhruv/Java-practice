d21f4b6ba2dce9ce588363ed4c35e92dc0c7dc56
Dhruv Desai <dhruvshah28@gmail.com>
2020-07-10 17:58:47 +0530

    Palindrome Updated

--- Dhruv/Strings/Palindrome.java
+++ Dhruv/Strings/Palindrome.java
@@ -1,9 +1,11 @@
+package Strings;
 import java.util.*;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter your String to check if it is a Palindrome or not:");
         String str = s.next();
+        str = str.toLowerCase();
         String reverse = "";
         for (int i = str.length() - 1; i >= 0; i--) {
             reverse = reverse + str.charAt(i);
