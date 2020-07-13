ab2f34f64282942ac5df1d5f926f033029713275
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-07-13 19:59:47 +0530

    Basics

---
+++ Hrithik/String/Basics.java
@@ -0,0 +1,13 @@
+import java.util.*;
+/**
+ * Basics
+ */
+public class Basics {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a String:");  
+    String name = sc.nextLine();
+    System.out.println(name);
+    sc.close();
+}
+}
