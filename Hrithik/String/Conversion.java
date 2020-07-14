b412da09559e52be65cb2dc482885e24b2b776a6
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-07-14 20:29:41 +0530

    Conversion

---
+++ Hrithik/String/Conversion.java
@@ -0,0 +1,12 @@
+public class Conversion {
+    public static void main(String[] args) {
+        String x = "5";
+        int y = 50;
+
+        String s1 = String.valueOf(x);
+        System.out.println(s1);
+
+        int temp = Integer.valueOf(y);
+        System.out.println(temp);
+    }
+}
