89a186778a48e52ad4c86dc79bee3733853454f9
Dhruv Desai <dhruvshah28@gmail.com>
2020-07-14 19:56:30 +0530

    String Methods Updated

--- Dhruv/Strings/PrintString.java
+++ Dhruv/Strings/PrintString.java
@@ -1,3 +1,17 @@
+// Methods to use in String class:
+/* 
+1. char charAt(int index)
+2. int length()
+3. String substring(int begIndex)
+4. String substring(int begIndex, int endIndex)
+5. boolean contains(CharSequence s)
+6. boolean equals(Object obj)
+7. boolean isEmpty()
+8. String concat(String str)
+9. String replace(char old, char new)
+10. String[] split(String regex)
+*/
+
 package Strings;
 import java.util.*;
 public class PrintString {
