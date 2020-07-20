4f94e3deed535f7ebc6b924cf9ca01ae0a75dbb7
Dhruv Desai <dhruvshah28@gmail.com>
2020-07-20 23:34:26 +0530

    LinkedList Updated

---
+++ Dhruv/LinkedList/BasicLinkedList.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+public class BasicLinkedList {
+    Node head; // Head of the list
+
+    // Linkded List Node
+    static class Node {
+        int data;
+        Node next;
+    // Constructor to create a new node 
+    // Next is by default intitalized as null
+        Node(int d) {
+            data = d;
+        }
+    }
+}
