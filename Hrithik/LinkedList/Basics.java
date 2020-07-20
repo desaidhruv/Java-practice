705be1899d6533f4b913210689f9b333fb55b011
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-07-20 23:23:56 +0530

    Basic Structure of Node

---
+++ Hrithik/LinkedList/Basics.java
@@ -0,0 +1,15 @@
+
+public class Basics {
+    Node head; // Head of the list
+
+    // Linkded List Node
+    static class Node {
+        int data;
+        Node next;
+    // Constructor to create a new node 
+    // Next is by default intitalized as null
+        Node(int d) {
+            data = d;
+        }
+    }
+}
