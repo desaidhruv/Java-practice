dd0548759a9b8a9b017802c3bcda29a33cbdc9cc
Dhruv Desai <dhruvshah28@gmail.com>
2020-07-20 09:26:20 +0530

    IntToBinary Updated

---
+++ Dhruv/Strings/IntToBinary.java
@@ -0,0 +1,25 @@
+package Strings;
+import java.util.*;
+public class IntToBinary {
+    public static void main(String[] args) {
+        int j = 5;        
+        String s=Integer.toBinaryString(j);;
+        int op = 0;
+         int count = 0;
+         for(int i = 0;i<s.length();i++)
+         {
+             if(s.charAt(i) == '1')
+             {
+                 count++;
+                 if(count > op)
+                     op = count;
+             }
+             else
+             {
+                 count = 0;
+             }
+         }
+         
+         System.out.println(op);
+    }
+}
