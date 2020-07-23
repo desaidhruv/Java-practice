6368fd661cf1c6665efd7ec66bfe571bc16ed9ba
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2020-07-23 23:16:08 +0530

    Updated LinkedList with all Functions

--- Hrithik/LinkedList/LinkedLists.java
+++ Hrithik/LinkedList/LinkedLists.java
@@ -41,7 +41,7 @@ public class LinkedLists {
         if(index == 0){
             insertAtStart(data);
         }
-
+        else {
         Node n = head;
         // Traverse till the index value
         for(int i = 0; i < index - 1; i++){
@@ -51,7 +51,34 @@ public class LinkedLists {
         new_node.next = n.next;
         // New node is pointed by the previous node
         n.next = new_node;
+        }
+    }
 
+    // 
+    public void deleteAt(int index){
+        if(index == 0){
+            head = head.next;
+        } else {
+            Node n = head;
+            Node n1 = null;
+            for(int i = 0 ; i < index-1; i++){
+                n = n.next;
+            }
+            n1 = n.next; // It refers to the next node after the the index node || the node we want ot delete.
+            n.next = n1.next; // Now we assign the address of next node to the index node.
+            System.out.println("The deleted node is " + n1.data);
+        }
+    }
+    public void Size(){
+        Node n = head;
+        int count = 0;
+
+        while(n != null){
+            count++;
+            n = n.next;
+        }
+        System.out.println();
+        System.out.println("The size of list is " + count);
     }
 
     public void show(){
@@ -66,11 +93,13 @@ public class LinkedLists {
     public static void main(String[] args) {
         LinkedLists list = new LinkedLists();
         list.insert(5);
-        list.insert(45);
         list.insert(34);
-        list.insertAtStart(10);
+        list.insertAtStart(20);
         list.insertAt(2,18);
+        list.insertAt(3,58);
+        list.deleteAt(2);
 
         list.show();
+        list.Size();
     }
 }
