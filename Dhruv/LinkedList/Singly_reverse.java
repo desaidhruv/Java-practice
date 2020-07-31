b0e475b8a97e26c76fccaef7bfa3d54dd382e598
Dhruv Desai <dhruvshah28@gmail.com>
2020-07-31 20:01:51 +0530

    Singly Reverse

---
+++ Dhruv/LinkedList/Singly_reverse.java
@@ -0,0 +1,54 @@
+package LinkedList;
+
+public class Singly_reverse {
+    private Node head;
+    private static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }       
+    }
+    public Node reverse(Node head) {
+        Node current = head;
+        Node previous = null;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+    public void display(Node head) {
+        if (head == null) {
+            return;
+        }
+
+        Node current = head;
+
+        while(current != null) {
+            System.out.print(current.data+ " --> "); // print current element's data.
+            current = current.next;
+        }
+        System.out.print(current); // here current will be null
+    }
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        Node second = new Node(8);
+        Node third = new Node(4);
+        Node fourth = new Node(12);
+
+        // Attach them together
+        head.next = second; // 10 --> 8
+        second.next = third; // 8 --> 4
+        third.next = fourth; // 4 --> 12
+        Singly_reverse list = new Singly_reverse();
+        list.display(head);
+        Node newHead = list.reverse(head);
+        System.out.println();
+        list.display(newHead);
+    }
+}
