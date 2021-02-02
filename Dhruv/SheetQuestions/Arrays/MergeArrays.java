eedcd34af19cf8860d697a8459c4250a0b2bbd23
Dhruv Desai <dhruvshah28@gmail.com>
2021-02-03 00:50:07 +0530

    Merge Arrays Optimised.

--- Dhruv/SheetQuestions/Arrays/MergeArrays.java
+++ Dhruv/SheetQuestions/Arrays/MergeArrays.java
@@ -2,38 +2,43 @@ package SheetQuestions.Arrays;
 import java.util.*;
 public class MergeArrays {
     static void MergeArray(int arr1[],int arr2[]) {
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        ArrayList<Integer> merged = new ArrayList<Integer>();
-        for(int i = 0;i < arr1.length;i++) {
-            merged.add(arr1[i]);
+        int n = arr1.length;
+        int m = arr2.length;
+        // Consider each element `arr1[i]` of array `arr1` and ignore the element if it is
+        // already in the correct order; otherwise, swap it with the next smaller
+        // element, which happens to be the first element of `arr2`.
+        for(int i = 0; i < n; i++) {
+            // compare the current element of `arr1[]` with the first element of `arr2[]`
+            if(arr1[i] > arr2[0]) {
+                // swap `arr1[i] with `arr2[0]`
+                int temp = arr1[i];
+                arr1[i] = arr2[0];
+                arr2[0] = temp;
+            }
+            int first = arr2[0];
+
+            int k;
+            // move `arr2[0]` to its correct position to maintain the sorted
+            // order of `arr2[]`.
+            for(k = 1; k < m && arr2[k] < first;k++) {
+                arr2[k-1] = arr2[k];
+            }
+            arr2[k - 1] = first;
         }
-        for(int i = 0;i < arr2.length;i++) {
-            merged.add(arr2[i]);
-        }
-        Integer[] arr3 = new Integer[merged.size()];
-        arr3 = merged.toArray(arr3);
-        for(Integer x: arr3) {
-            System.out.print(x + " ");
+    }
+    static void printArray(int arr[]) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array1: ");
-        int n = sc.nextInt();
-        int[] arr1 = new int[n];
-        for (int i = 0; i < n; i++) {
-            System.out.println("Enter array1 elements: ");
-            arr1[i] = sc.nextInt();
-        }
-        System.out.println("Enter the size of the array2: ");
-        int m = sc.nextInt();
-        int[] arr2 = new int[n];
-        for (int i = 0; i < m; i++) {
-            System.out.println("Enter array2 elements: ");
-            arr2[i] = sc.nextInt();
-        }
+        int[] arr1 = {1,4,7,8,10};
+        int[] arr2 = {2,3,9};
         MergeArray(arr1,arr2);
+        printArray(arr1);
+        System.out.println();
+        printArray(arr2);
         sc.close();
     }
 }
