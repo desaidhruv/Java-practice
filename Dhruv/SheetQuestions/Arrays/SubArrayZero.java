baa3d1a7b0b1d76a8a7c51a340a82bc97e72250b
Dhruv Desai <dhruvshah28@gmail.com>
2021-02-11 20:26:38 +0530

    SubArrayZero

---
+++ Dhruv/SheetQuestions/Arrays/SubArrayZero.java
@@ -0,0 +1,53 @@
+package SheetQuestions.Arrays;
+import java.util.*;
+
+public class SubArrayZero {
+    static void UnOpSubArrayExists(int arr[], int n) {
+        int flag = 0;
+        for(int i = 0; i < n; i++) {
+            int s = 0;
+            for(int j=i; j < n; j++) {
+                s = s + arr[j];
+                if(s == 0) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 1 ) {
+                break;
+            }
+        }
+        if(flag == 1) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+    static void OpSubArrayExists(int arr[], int n) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        int flag=0,s=0;
+        for(int i = 0; i < n; i++) {
+            s = s + arr[i];
+            if(s == 0 || arr[i] == 0 || (!res.contains(arr[i]))) {
+                flag = 1;
+                break;
+            }
+            else {
+                res.add(arr[i]);
+            }
+        }
+        if(flag == 1) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {4,2,-3,1,6};
+        int n = arr.length;
+        UnOpSubArrayExists(arr,n);
+        OpSubArrayExists(arr,n);
+    }    
+}
