6ed1cdb9671fdddaa63f1978ca5941f382e98268
Dhruv Desai <dhruvshah28@gmail.com>
2021-04-12 19:47:03 +0530

    Pattern Updated

---
+++ Dhruv/Pattern.java
@@ -0,0 +1,14 @@
+public class Pattern {
+    public static void printStars(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        printStars(n);
+    }
+}
