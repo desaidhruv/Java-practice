1e67b29eea7915738aca73efb1fe66317f80a1f7
Dhruv Desai <dhruvshah28@gmail.com>
2021-07-15 12:42:44 +0530

    SubArray Greater Than X Updated

--- Dhruv/SheetQuestions1/Arrays/SubarrayGreaterThanX.java
+++ Dhruv/SheetQuestions1/Arrays/SubarrayGreaterThanX.java
@@ -1,15 +1,25 @@
 package SheetQuestions1.Arrays;
 
 public class SubarrayGreaterThanX {
-    static void findSubarray(int[] array, int X) {
-        int[] array1;
-        for (int i = 0; i < array.length; i++) {
-            
+    static void findSubarray(int[] array, int X, int length) {
+        int start = 0, end = 0, ans = length+1, sum = 0;
+        while( end < length) {
+            while (sum < X && end < length) {
+                sum += array[end];
+                end++;
+            }
+            while (sum > ans && start < length) {
+                ans = Math.min(ans, end-start);
+                sum -= array[start];
+                start++;
+            }
         }
+        System.out.println(ans);
     }
     public static void main(String[] args) {
         int array[] =  {1, 4, 45, 6, 0, 19};
         int X = 51;
-        findSubarray(array,X);
+        int length = array.length;
+        findSubarray(array, X, length);
     }
 }
